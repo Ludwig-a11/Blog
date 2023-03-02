08293cca60c665ea340929306624a49674823a8e
Luis Zarza <boue123@gmail.com>
2023-03-02 00:12:49 -0600

    starting top bar section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TopBar from './components/TopBar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <TopBar />
+    
+  )
+}
+
+export default App
